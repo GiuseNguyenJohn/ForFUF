a0d50ba5f37b16ccb20ec42ad1b0d65054ba7e99
Magicks52 <72513899+Magicks52@users.noreply.github.com>
2022-02-09 05:56:38 +0000

    Added docstring and TODO list to ForFUF.py

---
+++ ForFUF.py
@@ -0,0 +1,17 @@
+"""
+Name: John Nguyen
+Contributor: Matt Sprengel
+Description: Automates basic checks for CTF forensics challenges
+Dependecies: binwalk, exiftool, hexdump, 
+Tested: Python 3.9 on Kali Linux
+"""
+
+"""
+TODO:
+- Add one-liner in README to install all dependencies in bash
+- Add function to 
+- Make argparser to parse:
+    - infile
+    - outfile
+    - 
+"""
