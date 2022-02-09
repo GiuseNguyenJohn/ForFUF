9ae59b0f8928664da1bb8f2275b9f2d6ba68208d
Magicks52 <72513899+Magicks52@users.noreply.github.com>
2022-02-09 18:45:23 +0000

    Updated ideas, todo

--- ForFUF.py
+++ ForFUF.py
@@ -2,16 +2,21 @@
 Name: John Nguyen
 Contributor: Matt Sprengel
 Description: Automates basic checks for CTF forensics challenges
-Dependecies: binwalk, exiftool, hexdump, 
+Dependecies: binwalk, exiftool, hexdump, zsteg, strings
 Tested: Python 3.9.6 on GNU/Linux
 """
 
 """
 Ideas:
-- how to format output? files? stdout?
+- Questions:
+    - grep for flag, or parse with python?
+    - how to format output? files? stdout?
 - automatic flag detection?
 - check and fix corrupt file headers
-- 
+- check LSB steg with zsteg
+- extract strings and search for flag
+- use regex to identify when flag characters are all grouped together within
+    a certain range
 """
 
 """
@@ -21,8 +26,12 @@ TODO:
 - Make argparser to parse:
     - infile
     - outfile
+    - flag format
+    - fake flag format?
     - 'all' option
     - individual checks only:
-        - binwalk
+        - embedded files
+        - metadata
+        - LSB encoding
         - 
 """
