de79ee480da32311583ba14fbe852ba05d772a23
Magicks52 <72513899+Magicks52@users.noreply.github.com>
2022-02-11 12:32:56 -0800

    added test_check_file_exists

--- forfuf_unit_tests.py
+++ forfuf_unit_tests.py
@@ -1,4 +1,13 @@
 import unittest
 import forfuf
 
-class forfuf
+class ForfufTestCase(unittest.TestCase):
+    """Tests for 'forfuf.py'."""
+
+    def test_check_file_exists(self):
+        """Will filename 'test.png' (exists) return True?"""
+        file_exists = forfuf.check_file_exists('test.png')
+        self.assertEqual(file_exists, True)
+
+if __name__ = '__main__':
+    unittest.main()
