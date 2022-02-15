18e93a9b7287684f921a81b003e1ba6def1ad3b3
Magicks52 <72513899+Magicks52@users.noreply.github.com>
2022-02-14 19:34:44 -0800

    fixed test_range_parsing

--- forfuf_unit_tests.py
+++ forfuf_unit_tests.py
@@ -74,7 +74,7 @@ class ForfufTestCase(unittest.TestCase):
 
     def test_range_parsing(self):
         """Will match object and exif data input return flag?"""
-        match_object = forfuf.get_regex_flag_format(r"i.{0,2}c.{0,2}t.{0,2}f.{0,2}\{.*\}")
+        match_object = forfuf.get_regex_flag_format(r"p.{0,2}i.{0,2}c.{0,2}t.{0,2}f.{0,2}\{.*\}")
         correct_flag = "ictf{flag}"
         found_flag = forfuf.parse_for_possible_flags(match_object, text_for_test_parsing)
         self.assertIn(correct_flag, found_flag)
