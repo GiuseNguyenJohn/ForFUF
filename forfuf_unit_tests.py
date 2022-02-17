3dfa5c49dcef86a094811eb14e4a25879ccdfb78
Magicks52 <72513899+Magicks52@users.noreply.github.com>
2022-02-17 10:27:39 -0800

    fixed unit test (uncomfirmed)

--- forfuf_unit_tests.py
+++ forfuf_unit_tests.py
@@ -21,14 +21,14 @@ Resolution Unit                 :
 X Resolution                    : 100
 Y Resolution                    : 100
 Current IPTC Digest             : 0def9dc9a98ce8bc8abdd7b2c54c23f2
-Copyright Notice                : picoCTF{1337}
+Copyright Notice                : picoCTF{1337} 
 Application Record Version      : 4
 XMP Toolkit                     : Image::ExifTool 12.40
-Rights                          : cvpbPGS{1337}
+Rights                          : cvpbPGS{1337} 
 Image Width                     : 2370
 Image Height                    : 1927
 Encoding Process                : Baseline DCT, Huffman coding
-Bits Per Sample                 : cGljb0NURnsxMzM3fQ==
+Bits Per Sample                 : cGljb0NURnsxMzM3fQ== 
 Color Components                : .p.i.c.o.C.T.F.{.f.l.a.g.}.
 Y Cb Cr Sub Sampling            : YCbCr4:4:4 (1 1)
 Image Size                      : 2370x1927
@@ -45,20 +45,20 @@ class ForfufTestCase(unittest.TestCase):
 
     def test_parse_for_possible_flags(self):
         """Will --flag-format and --start-flag return correct flags?"""
-        plaintext_mo, rot_13_mo, base64_mo = forfuf.get_regex_flag_format("p.{0,2}i.{0,2}c"
+        plaintext_mo, rot_13_mo, base64_mo = forfuf.get_regex_flag_formats("p.{0,2}i.{0,2}c"
                 ".{0,2}o.{0,2}C.{0,2}T.{0,2}F.{0,2}{.*}", "picoCTF{")
         # correct flags
-        correct_plaintext_flag = "picoctf{1337}"
+        correct_plaintext_flag = "picoCTF{1337}"
         correct_range_flag = "p.i.c.o.C.T.F.{.f.l.a.g.}"
-        correct_rot_13_flag = ["cvpbpgs{1337}"]
-        correct_base64_flag = ["cGljb2N0ZnsxMzM3fQ== "] # space at the end because regex will match space too
+        correct_rot_13_flag = ["cvpbPGS{1337}"]
+        correct_base64_flag = ["cGljb0NURnsxMzM3fQ== "] # space at the end because regex will match space too
         # found flags
-        plaintext_flags = parse_for_possible_flags(plaintext_mo, text_for_test_parsing)
-        rot_13_flags = parse_for_possible_flags(rot_13_mo, text_for_test_parsing)
-        base64_flags = parse_for_possible_flags(base64_mo, text_for_test_parsing)
+        plaintext_flags = forfuf.parse_for_possible_flags(plaintext_mo, text_for_test_parsing)
+        rot_13_flags = forfuf.parse_for_possible_flags(rot_13_mo, text_for_test_parsing)
+        base64_flags = forfuf.parse_for_possible_flags(base64_mo, text_for_test_parsing)
         # compare found flags to correct ones
         self.assertEquals(correct_plaintext_flag, plaintext_flags[0])
-        self.assertEquals(correct_plaintext_flag, plaintext_flags[1])
+        self.assertEquals(correct_range_flag, plaintext_flags[1])
         self.assertEquals(correct_rot_13_flag, rot_13_flags)
         self.assertEquals(correct_base64_flag, base64_flags)
 
