0f45669ea97aa3388a5cd2150e61fd97cd296b4e
Magicks52 <72513899+Magicks52@users.noreply.github.com>
2022-02-16 16:35:03 -0800

    fixed base64 regex (hopefully)

--- forfuf.py
+++ forfuf.py
@@ -67,8 +67,8 @@ def get_regex_flag_formats(regex_string, start_flag):
     # Pattern of plaintext, rot13, and base64
     plaintext_pattern = re.compile(regex_string)
     rot13_pattern = re.compile(codecs.encode(regex_string, 'rot-13'))
-    base64_first_three = base64.b64encode(bytes(start_flag, 'utf-8'))
-    base64_pattern = re.compile(f"{base64_first_three[0:3]}[A-Za-z0-9+\]+[=]{0,2}")
+    base64_first_three = base64.b64encode(bytes(start_flag, 'utf-8')).decode()
+    base64_pattern = re.compile(f"{base64_first_three[0:3]}[A-Za-z0-9+\]+[=]{{0,2}}")
     return plaintext_pattern, rot13_pattern, base64_pattern
 
 def parse_for_possible_flags(match_object, text):
